60ffaa1aa3dc5b13340d23ee357d1901dc0e36b5
Joshesol <joshuaericsolano@gmail.com>
2026-01-14 10:46:53 -0700

    added comments

--- Mission2HW/Program.cs
+++ Mission2HW/Program.cs
@@ -7,35 +7,51 @@ public class Program
 
         Console.WriteLine("Welcome to the dice throwing simulator!\n");
 
+        // create rolls starting as 0
         int rolls = 0;
 
+        // ask user how many roles to do
         Console.WriteLine("How many dice rolls would you like to simulate? ");
         rolls = int.Parse(Console.ReadLine());
 
+        // simulate the a roll how ever many times the user wanted to roll, and store the 
+        // combinations in the variable "rollResults"
         int[] rollResults = roll.RollingSimulation(rolls);
 
+        // display results
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents " +
                           "1% of the total number of rolls.\nTotal number " +
                           "of rolls = " + rollResults.Length + ".\n");
 
+        // create empty array with as many slots as possible combinations (11 because cant combo to 1)
         int[] counts = new int[11];
 
+        // increment each combination based on index
         foreach (int i in rollResults)
         {
             counts[i - 2]++;
         }
 
+        // set default first display num to 2, will increment thus on
         int rollNum = 2;
+        
+        // print the counts
         foreach (int i in counts)
         {
+            // calculate the percent of how frequent that index/combo occured 
             double percent = ((double)i / rollResults.Length) * 100;
             
+            // create a string for the histogram that shows a * for each percent
             string bars = new string('*', (int)percent);
             
+            // print the percent *'s
             Console.WriteLine(rollNum + ": " + bars);
+            
+            // increment
             rollNum++;
         }
         
+        // adios, amigo
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
 
     }
--- Mission2HW/Roll.cs
+++ Mission2HW/Roll.cs
@@ -2,28 +2,35 @@ namespace Mission2HW;
 
 public class Roll
 {
+    // Simulates a full roll
     public int[] RollingSimulation(int rolls)
     {
+        // create final roll combination array that will be returned later (long as rolls requested) 
         int[] combinations = new int[rolls];
         
+        // for each roll, roll twice
         for (int roll = 0; roll < combinations.Length; roll++)
         {
+            // the two roles
             int rol1 = RollSimulator();
             int rol2 = RollSimulator();
             
+            // create and return roll combination
             int combination = rol1 + rol2;
-            
             combinations[roll] = combination;
         }
         
         return combinations;
     }
 
+    // simulates one individual die roll
     public int RollSimulator()
     {
+        // roll is a random result of min as 1 and max as 6
         Random rnd = new Random();
         int result = rnd.Next(1, 7);
 
+        // return result
         return result;
     }
 }
